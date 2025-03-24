e163acc69d2c600aa71d5600926176bc97c170f6
Daniel Schaefer <dhs@frame.work>
2025-03-25 01:41:38 +0800

    framework_tool: Hardcode to turn off 8 ARGB on
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- framework_tool/src/main.rs
+++ framework_tool/src/main.rs
@@ -6,7 +6,21 @@ fn get_args() -> Vec<String> {
 }
 
 fn main() -> Result<(), &'static str> {
-    let args = commandline::parse(&get_args());
+    //let args = get_args();
+    let args: Vec<String> = [
+        "framework_tool",
+        "--rgbkbd",
+        "0",
+        "0xFFFFFF",
+        "0xFFFFFF",
+        "0xFFFFFF",
+        "0xFFFFFF",
+        "0xFFFFFF",
+        "0xFFFFFF",
+        "0xFFFFFF",
+        "0xFFFFFF",
+    ].iter().map(|x| x.to_string()).collect();
+    let args = commandline::parse(&args);
     if (commandline::run_with_args(&args, false)) != 0 {
         return Err("Fail");
     }
